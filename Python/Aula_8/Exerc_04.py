7ede996f801190db12b030525045e82abe77a3c3
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-25 19:54:55 -0300

    Create Exerc_04.py

---
+++ Python/Aula_8/Exerc_04.py
@@ -0,0 +1,9 @@
+lista_nome = []
+for x in range(5):
+    lista_nome.append(input('Digite um nome: '))
+print()
+for z in range(5):
+    print(lista_nome[z])
+print()
+for y in range(-1, -6, -1):
+    print(lista_nome[y])
