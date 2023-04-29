1bf5ddc92636f0c6331f232ff372f03130732582
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-29 00:35:56 -0300

    Delete Exerc_4.py

--- Python/Aua_5/Exerc_4.py
+++
@@ -1,9 +0,0 @@
-entre_10_20 = 0
-fora_10_20 = 0
-for num in range(10):
-    perg = int(input('Digite um valor: '))
-    if 10 <= perg <= 20:
-        entre_10_20 += 1
-    else:
-        fora_10_20 += 1
-print(f'Nesses valores passados {entre_10_20} foram entre 10 e 20. {fora_10_20} não foram.')
