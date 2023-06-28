29cd3d9096bffb5faa5711a61ec4611d86437ab1
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-27 22:13:16 -0300

    Create Exerc_05.java

---
+++ Java/Aula_01/Exerc_05.java
@@ -0,0 +1,20 @@
+package aula_1;
+
+public class Exerc_5 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int c = 7;
+		
+		if (a++ >= b) {
+			System.out.println(--c);
+		}
+		
+		else {
+			System.out.println(c++);
+		}
+
+	}
+
+}
