24acdb457ea9da473979e79e8f4c4c502b5c8f9b
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-27 22:13:04 -0300

    Create Exerc_01.java

---
+++ Java/Aula_01/Exerc_01.java
@@ -0,0 +1,13 @@
+package aula_1;
+
+public class Exerc_1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		
+		System.out.println(a++ + ++a);
+		System.out.println(b++ + b++);
+	}
+
+}
