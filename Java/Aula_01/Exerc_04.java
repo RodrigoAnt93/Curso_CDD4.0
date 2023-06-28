ad2c07b64eb6212e6f73b47889f25bfc4375e16e
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-27 22:13:13 -0300

    Create Exerc_04.java

---
+++ Java/Aula_01/Exerc_04.java
@@ -0,0 +1,14 @@
+package aula_1;
+
+public class Exerc_4 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int c = 7;
+		
+		System.out.println(!((a > b) && (a < c)));
+
+	}
+
+}
