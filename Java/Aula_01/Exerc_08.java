042f66eb8ff601d286c638abc3d0845aecd6e187
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-28 22:19:48 -0300

    Create Exerc_08.java

---
+++ Java/Aula_01/Exerc_08.java
@@ -0,0 +1,17 @@
+package aula_1;
+
+import java.util.Scanner;
+
+public class Exerc_8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um numero: ");
+		double resp = sc.nextDouble();
+		System.out.println(resp);
+		
+		sc.close();
+	}
+
+}
