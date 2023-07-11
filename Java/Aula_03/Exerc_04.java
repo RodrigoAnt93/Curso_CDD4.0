a89760349e1db39ad3ca3b171bae96cfb6f8ddc6
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-07-10 22:09:47 -0300

    Create Exerc_04.java

---
+++ Java/Aula_03/Exerc_04.java
@@ -0,0 +1,13 @@
+package aula_3;
+
+public class Exerc_04 {
+	public static void main(String[] args) {
+		
+	String txt1 = "Será que são iguais";
+	String txt2 = "Será que são iguais";
+	
+	System.out.println(txt1.compareTo(txt2) == 0 ? true : false);
+	
+	}
+
+}
