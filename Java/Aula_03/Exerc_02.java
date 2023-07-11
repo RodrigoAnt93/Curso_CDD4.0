4ba55407cbc5d9a2d328267f9a15bd2cab7acf16
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-07-10 22:09:40 -0300

    Create Exerc_02.java

---
+++ Java/Aula_03/Exerc_02.java
@@ -0,0 +1,14 @@
+package aula_3;
+
+public class Exerc_02 {
+
+	public static void main(String[] args) {
+		String txt = " texto para retirar espaços no inicio e fim ";
+		System.out.println(txt.trim());
+		System.out.println(txt.strip());
+		
+		System.out.println();
+
+	}
+
+}
