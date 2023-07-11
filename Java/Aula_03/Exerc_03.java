8da72bdacc82589d130e594d612601cda325c8a4
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-07-10 22:09:44 -0300

    Create Exerc_03.java

---
+++ Java/Aula_03/Exerc_03.java
@@ -0,0 +1,13 @@
+package aula_3;
+
+import java.util.StringTokenizer;
+
+public class Exerc_03 {
+
+	public static void main(String[] args) {
+		StringTokenizer str = new StringTokenizer("A cidade não para, a cidade só cresce.");
+		System.out.println(str.countTokens());
+
+	}
+
+}
